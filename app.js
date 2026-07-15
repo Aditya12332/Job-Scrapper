9721b38638fdb62326e75bc745c3a71fd0054659
Aditya <adityasultania123321@gmail.com>
2026-07-16 03:23:43 +0530

    main app file

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 require("dotenv").config();
-
+const connectDB=require("./src/database/mongo");
 const {
     loadCompanies,
     loadKeywords
 } = require("./src/config/config");
 
-const companies = loadCompanies();
+const fetchWorkdayJobs = require("./src/ats/workday");
+const parseJobs = require("./src/ats/workday/parser");
+const { filterJobs } = require("./src/services/filter");
 
-const keywords = loadKeywords();
-const validateCompany = require("./src/utils/validateCompany");
+async function main() {
+    await connectDB();
+    const companies = loadCompanies();
+    const keywords = loadKeywords();
 
-companies.forEach(validateCompany);
+    const raw = await fetchWorkdayJobs(companies[0]);
 
-console.log("Configuration Loaded Successfully");
+    const jobs = parseJobs(companies[0], raw);
+
+    const filtered = filterJobs(jobs, keywords);
+
+    console.log(filtered);
+}
+
+main();
