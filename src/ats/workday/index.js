2f24c9066bb3cad5be554ece6d756fe2e7fd509a
Aditya <adityasultania123321@gmail.com>
2026-07-16 20:57:28 +0530

    workday ats tracker

--- src/ats/workday/index.js
+++ src/ats/workday/index.js
@@ -1,40 +1,37 @@
 const axios = require("axios");
 
 async function fetchWorkdayJobs(company) {
-
     try {
-
         const response = await axios.post(
-
             company.endpoint,
-
             {
                 appliedFacets: {},
                 limit: 20,
                 offset: 0,
                 searchText: ""
             },
-
             {
                 headers: {
-                    "Content-Type": "application/json"
-                }
+                    "Content-Type": "application/json",
+                    "Accept": "application/json"
+                },
+                timeout: 15000
             }
-
         );
 
         return response.data;
+    } catch (err) {
+        console.error(`Failed to fetch ${company.company}`);
 
-    }
-
-    catch (err) {
-
-        console.error(err.response?.data || err.message);
+        if (err.response) {
+            console.error(err.response.status);
+            console.error(err.response.data);
+        } else {
+            console.error(err.message);
+        }
 
         return null;
-
     }
-
 }
 
 module.exports = fetchWorkdayJobs;
